e1e2bda90154648b093a2e44e5b9cba78e26a38d
SIST <SIST@DESKTOP-VNU78PV>
2023-10-31 18:31:01 +0900

    성부 - 굿즈 서블릿 수정

--- AgencyProducts_Project/src/main/java/com/goods/GoodsServlet.java
+++ AgencyProducts_Project/src/main/java/com/goods/GoodsServlet.java
@@ -13,7 +13,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.servlet.http.Part;
 
 import com.util.FileManager;
 import com.util.MyUploadServlet;
